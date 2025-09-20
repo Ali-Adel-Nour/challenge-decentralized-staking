e1d826ba80e5e9f79280ff8edd1e5bc6fb03ffe5
a.kenawy@v4tech.com <a.kenawy@v4tech.com>
2025-09-20 04:26:31 +0300

    formatting and removing console

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-import "hardhat/console.sol";
+pragma solidity 0.8.30;
+
 import "./ExampleExternalContract.sol";
 
 contract Staker {
@@ -9,48 +9,44 @@ contract Staker {
     constructor(address exampleExternalContractAddress) {
         exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
     }
-   
 
-uint256 public constant threshold = 1 ether;
-uint256 public deadline = block.timestamp + 72 hours;  
-uint256 public startTime = block.timestamp;  
-mapping(address => uint256) public balances;
-event Staked(address user, uint256 amount);
+    uint256 public constant threshold = 1 ether;
+    uint256 public deadline = block.timestamp + 72 hours;
+    uint256 public startTime = block.timestamp;
+    mapping(address => uint256) public balances;
+    event Staked(address user, uint256 amount);
 
-
-function stake() public payable {
-    require(msg.value > 0, "Must send ETH to stake");
-    balances[msg.sender] += msg.value;
-    console.log("Staking:", msg.sender, msg.value);
-    emit Staked(msg.sender, msg.value);
-}
-
-function execute() external {
-  require(block.timestamp >= deadline, "Deadline not reached");
-    if (address(this).balance >= threshold) {
-        exampleExternalContract.complete{value: address(this).balance}();
+    function stake() public payable {
+        require(msg.value > 0, "Must send ETH to stake");
+        balances[msg.sender] += msg.value;
+        console.log("Staking:", msg.sender, msg.value);
+        // console.log("Staking:", msg.sender, msg.value);
     }
-}
 
-function withdraw() external {
-    require(block.timestamp >= deadline, "Deadline not reached");
-    require(address(this).balance < threshold, "Threshold met, cannot withdraw");
-    uint256 amount = balances[msg.sender];
-    require(amount > 0, "No balance to withdraw");
-    balances[msg.sender] = 0;
-    payable(msg.sender).transfer(amount);
-}
-
-
-function timeLeft() public view returns (uint256) {
-    if (block.timestamp >= deadline) {
-        return 0;
+    function execute() external {
+        require(block.timestamp >= deadline, "Deadline not reached");
+        if (address(this).balance >= threshold) {
+            exampleExternalContract.complete{ value: address(this).balance }();
+        }
     }
-    return deadline - block.timestamp;
-}
 
-receive() external payable {
-    stake();
-}
+    function withdraw() external {
+        require(block.timestamp >= deadline, "Deadline not reached");
+        require(address(this).balance < threshold, "Threshold met, cannot withdraw");
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "No balance to withdraw");
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
 
+    function timeLeft() public view returns (uint256) {
+        if (block.timestamp >= deadline) {
+            return 0;
+        }
+        return deadline - block.timestamp;
+    }
+
+    receive() external payable {
+        stake();
+    }
 }
